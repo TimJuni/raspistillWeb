b1b04e71353321029311d4e33ecb2f9fcdb45914
Tim Jungnickel <tim.jungnickel@gmail.com>
2013-10-07 08:33:07 +0200

    deleted README.txt and modified setup.py to use README.md

--- README.txt
+++
@@ -1,1 +0,0 @@
-raspistillWeb README
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.txt')) as f:
+with open(os.path.join(here, 'README.md')) as f:
     README = f.read()
 with open(os.path.join(here, 'CHANGES.txt')) as f:
     CHANGES = f.read()
@@ -18,7 +18,7 @@ requires = [
     ]
 
 setup(name='raspistillWeb',
-      version='0.0',
+      version='0.1',
       description='raspistillWeb',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
